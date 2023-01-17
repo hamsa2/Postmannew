36860d993651c395cab6d18c9556d66a134be666
Nailiya <66hamsa@gmail.com>
2023-01-17 12:59:54 +0700

    init

---
+++ src/test/java/PostmanTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+
+    @Test
+    void shouldReturneBody() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello!") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello!"));
+    }
+}
